55cb6a2002d4aa994c22a8703fd2b8d47aa944e4
Matan Anavi <MrBahur25@gmail.com>
2019-03-06 17:33:38 +0200

    opened and showed image

--- src/Demo.java
+++ src/Demo.java
@@ -1,9 +1,22 @@
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+
 
 public class Demo{
     static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
 
     public static void main(String args[]){
         System.out.println(Core.VERSION);
+        Mat img = Imgcodecs.imread("C:\\Users\\matan anavi\\Desktop\\programing\\Excercise\\Hole_Filling\\src\\test2.png",Imgcodecs.IMREAD_GRAYSCALE);
+        HighGui.namedWindow("check",HighGui.WINDOW_AUTOSIZE);
+        HighGui.imshow("check",img);
+        HighGui.waitKey();
+        HighGui.destroyWindow("check");
+
+
+        System.out.println("wait here");
     }
 }
