a8412be515e13294b69f02d1072fe8988a305732
Matan Anavi <MrBahur25@gmail.com>
2019-03-06 18:41:15 +0200

    also was able to close the program properlu this time

--- src/Demo.java
+++ src/Demo.java
@@ -4,13 +4,12 @@ import org.opencv.core.MatOfByte;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 
-
 public class Demo{
     static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
 
     public static void main(String args[]){
         System.out.println(Core.VERSION);
-        Mat img = Imgcodecs.imread("C:\\Users\\matan anavi\\Desktop\\programing\\Excercise\\Hole_Filling\\src\\test2.png",Imgcodecs.IMREAD_GRAYSCALE);
+        Mat img = Imgcodecs.imread("C:\\Users\\matan anavi\\Desktop\\programing\\Excercise\\Hole_Filling\\src\\test3.png",Imgcodecs.IMREAD_GRAYSCALE);
         Double [][] image = new Double[img.rows()][img.cols()];
         for (int i = 0; i <img.rows() ; i++) {
             for (int j = 0; j <img.cols() ; j++) {
@@ -22,23 +21,18 @@ public class Demo{
         HighGui.namedWindow("image1",HighGui.WINDOW_AUTOSIZE);
         HighGui.imshow("image1",img);
         HighGui.waitKey(0);
-        HighGui.destroyWindow("image1");
 
-        Mat img2 = new Mat(image.length-1,image[0].length-1,Imgcodecs.IMREAD_GRAYSCALE);
+        Mat img2 = new Mat(image.length,image[0].length,Imgcodecs.IMREAD_GRAYSCALE);
         for (int i = 0; i <image.length ; i++) {
             for (int j = 0; j < image[i].length; j++) {
-                double [] x = new double[1];
-                x[0]=image[i][j]*255;
-                img2.put(i,j,x);
-
+                img2.put(i,j,image[i][j]*255);
             }
         }
 
-
-        HighGui.namedWindow("image2",HighGui.WINDOW_AUTOSIZE);
-        HighGui.imshow("image2",img2);
+        HighGui.imshow("image1",img2);
         HighGui.waitKey(0);
         HighGui.destroyWindow("image2");
+        HighGui.waitKey(2);
 
     }
 }
